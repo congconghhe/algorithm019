f6e606e484f1685f28f5e442b93d6a8879acb55d
congconghe <806434694@qq.com>
2020-11-05 22:59:18 +0800

    homework

---
+++ Week_01/homework.c
@@ -0,0 +1,21 @@
+int *plusOne(int *digits, int digitsSize, int *returnSize) {
+    for (int i = digitsSize - 1; i >= 0; --i) {
+        if (digits[i] == 9) {
+            digits[i] = 0;
+           //  *returnSize = digitsSize;  
+        } else {
+            digits[i]++;
+            *returnSize = digitsSize;
+             return digits;
+        }
+    }
+    int * final = (int *)malloc(sizeof(int)*(digitsSize+1));
+    final[0]=1;
+    for(int i = 1;i<digitsSize+1;i++){
+        final[i]=digits[i-1];
+    }
+     *returnSize = digitsSize +1;
+   return final;
+ }
+
+
